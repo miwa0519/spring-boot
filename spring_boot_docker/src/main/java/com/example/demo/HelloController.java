2d221a9db281eb39c77e7ea7c235412c41475bc5
h_miwa <haruka_miwa@pure-system.com>
2019-06-04 18:09:56 +0900

    Viewの追加とControllerの修正

--- spring_boot_docker/src/main/java/com/example/demo/HelloController.java
+++ spring_boot_docker/src/main/java/com/example/demo/HelloController.java
@@ -3,10 +3,10 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
   @RequestMapping("/hello")
   public String hello() {
-    return "Hello World!";
+    return "hello";
   }
 }
